5f3bc71981c896055aaedfc79ac583f4ad2af42e
Roshan Nagekar <roshangn88@gmail.com>
2015-07-17 10:49:12 +0530

    Corrected environment variable

--- config/nginx-start.sh
+++ config/nginx-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp /opt/etc/nginx.conf /etc/nginx/nginx.conf
 
-cp /data/${NGINX_VHOSTS} /etc/nginx/sites-available/
+cp /${data_root}/${NGINX_VHOSTS} /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/*.conf /etc/nginx/sites-enabled/
 
 sed -i "s/%fastgci-ip%/$PHP_PORT_9000_TCP_ADDR/" /etc/nginx/nginx.conf
