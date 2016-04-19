238cf75b97f36eec617142e3bc154210e30d3013
Andre Lackmann <andre@lackmann.net>
2016-04-19 13:32:47 +1000

    allowed ip restrictions to be supplied by env var

--- config/nginx-start.sh
+++ config/nginx-start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 NGINX_VHOSTS=${NGINX_VHOSTS:-/data/vhosts}
+NGINX_CONF=${NGINX_CONF}
 
 rm /etc/nginx/sites-available/* /etc/nginx/sites-enabled/*
 cp $NGINX_VHOSTS /etc/nginx/sites-available/
@@ -12,7 +13,35 @@ printenv | while read envdef; do #iterate over all env vars
     # replace all instances of %KEY% with the relevant environment value
     sed -i "s|%$key%|${!key}|" /etc/nginx/sites-available/*.conf
     sed -i "s|%$key%|${!key}|" /etc/nginx/nginx.conf # replace all instances of %KEY% with the value
+    sed -i "s|%$key%|${!key}|" /etc/nginx/nginx.conf # replace all instances of %KEY% with the value
+
+    # also do replacedments in addtional conf dir if it exists
+    if [ -d "$NGINX_CONF" ]; then
+        sed -i "s|%$key%|${!key}|" ${NGINX_CONF}/*.conf
+    fi
 done
+
+
+# dynamic ip restriction. Will create a file name ip-restriction.conf in your NGINX_CONF directory
+# include this file within your vhosts as required.
+# use the NGINX_ALLOWED_IPS env to pass in a list of allowed ip addresses in the format: 1.1.1.1;2.2.2.2;3.3.3.3; etc
+
+IP_CONF=${NGINX_CONF}/ip-restriction.conf
+ALLOWED_IPS=${NGINX_ALLOWED_IPS}
+if [ ${ALLOWED_IPS:?}  ]; then
+
+    echo "satisfy any;" > $IP_CONF
+    echo "error_page 403 = @deny;" >> $IP_CONF
+
+
+    ips=$(echo $ALLOWED_IPS | tr ";" "\n")
+    for ip in $ips
+    do
+        echo "allow $ip;" >> $IP_CONF
+    done
+
+    echo "deny all;" >> $IP_CONF
+fi
 
 cat /etc/nginx/sites-available/*.conf
 
